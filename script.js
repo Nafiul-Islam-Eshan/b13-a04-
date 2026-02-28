ee1f9c7d01ffb85c3a1cc17ce5052224f1b8ca9b
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-02-28 13:07:34 +0600

    working on job quantity functionalities

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+let totalCount = document.querySelector("#totalCount").innerText;
+let interviewCount = document.querySelector("#interviewCount").innerText;
+let rejectedCount = document.querySelector("#rejectedCount").innerText;
+
+const jobInfoCards = document.querySelector("#jobInfoCards");
+console.log(jobInfoCards);
